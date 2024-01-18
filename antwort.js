7bc8030655658d6ce37b032f162698ed026c1f4f
JensArp <donvito1978@googlemail.com>
2024-01-18 15:48:57 +0100

    erledigt

---
+++ antwort.js
@@ -0,0 +1,14 @@
+// Aufgabe 1
+document.getElementById("aufgabe_1").innerText = "Hello World";
+
+// Aufgabe 2
+var a = 2;
+var b = 3;
+var ergebnisAufgabe2 = a + b;
+document.getElementById("aufgabe_2").innerText = ergebnisAufgabe2;
+
+// Aufgabe 3
+var a2 = "Hello";
+var b2 = "World !";
+var ergebnisAufgabe3 = a2 + " " + b2;
+document.getElementById("aufgabe_3").innerText = ergebnisAufgabe3;
